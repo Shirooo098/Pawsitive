97b21807cbbcdfa83bcbc394224d4500383324d0
Shirooo098 <johndavid8803@gmail.com>
2025-04-14 04:06:21 +0800

    Minor CHange

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import bodyParser from "body-parser";
 import bcrypt from "bcrypt";
-import session from "express-session";
+import session from "cookie-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import adminRoutes from './routes/adminRoutes.js';
