2edcc66e9231175b348a845807f7ee3fab39e526
Shirooo098 <johndavid8803@gmail.com>
2025-04-14 11:58:17 +0800

    Fix pp issue

--- server/index.js
+++ server/index.js
@@ -79,7 +79,7 @@ app.use((req, res, next) => {
 app.use('/admin', adminRoutes);
 app.use('/', userRoutes);
 
-pp.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.json({  message: 'Backend is running' });
   });
 
