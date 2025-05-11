dfcef40472cec874454c459583e92c0dc558dd3f
Shirooo098 <johndavid8803@gmail.com>
2025-05-12 01:23:39 +0800

    Remove Unnecessary Lines

--- client/src/pages/user/HomePage.jsx
+++ client/src/pages/user/HomePage.jsx
@@ -1,13 +1,10 @@
-import { useEffect } from 'react';
+
 import Footer from '../../components/Footer';
 import Services from '../../components/Services';
 import Hero from '../../components/Hero';
 import Adoption from '../../components/Adoption';
 
 export default function HomePage() {
-  useEffect(() => {
-    // Carousel and scroll logic can be implemented here
-}, []);
 
 return (
     <div>
