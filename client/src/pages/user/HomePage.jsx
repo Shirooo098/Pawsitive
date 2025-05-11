8436cabd7a0301816667b5330cc8f1e981c515a3
Shirooo098 <johndavid8803@gmail.com>
2025-05-12 01:24:19 +0800

    Remove unnecesary lines

--- client/src/pages/user/HomePage.jsx
+++ client/src/pages/user/HomePage.jsx
@@ -1,11 +1,8 @@
-import { useEffect } from 'react';
+
 import Footer from '../../components/Footer';
 import Services from '../../components/Services';
 import Hero from '../../components/Hero';
 import Adoption from '../../components/Adoption';
-
-import './HomePage.css';
-
 
 export default function HomePage() {
 
