2e1409ed199c75f501bcd280950ed9e51e92712a
Shirooo098 <johndavid8803@gmail.com>
2025-05-12 03:45:25 +0800

    Minor Fix Backend

--- server/index.js
+++ server/index.js
@@ -62,7 +62,9 @@ app.use(session({
     resave: false,
     saveUninitialized: false, 
     cookie: {
-        maxAge: 24 * 60 * 60 * 1000 
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: true,
+        sameSite: 'none' 
     }
 }));
 
