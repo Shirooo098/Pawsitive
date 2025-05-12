caf8dd862d4d9ba2e0c8043c7ff0c179678ec742
Shirooo098 <johndavid8803@gmail.com>
2025-05-12 21:43:40 +0800

    Remove Line

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ const PgSession = pgSession(session);
 
 const client = process.env.FRONTEND_URL;
 
+
 // const db_user = process.env.POSTGRE_DB_USER;
 // const db_host = process.env.POSTGRE_HOST;
 // const db_name = process.env.POSTGRE_DB_NAME;
@@ -81,7 +82,6 @@ const dbConfig = {
     query_timeout: 10000
 };
 
-// Initialize database pool
 let db;
 let server;
 
