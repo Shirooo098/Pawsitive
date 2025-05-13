e455197746df130aaad1112857e72e1403159379
Shirooo098 <johndavid8803@gmail.com>
2025-05-14 03:29:29 +0800

    Add PATCH HTTPS method

--- server/index.js
+++ server/index.js
@@ -85,7 +85,7 @@ const corsOptions = {
         }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
     exposedHeaders: ['Content-Range', 'X-Content-Range'],
     maxAge: 86400 // 24 hours
